1c5cfbf98a01bbc6c26d7eef5122f37e58139dc1
Karo <hakobyan.karapet91@gmail.com>
2014-12-11 20:31:14 +0400

    Added

---
+++ js/infinite-slider.js
@@ -0,0 +1,186 @@
+/*
+*
+* Author Karo Hakobyan
+*
+*/
+
+"use strict";
+
+(function($){
+	
+	$.fn.infiniteSlider = function(options) {
+	
+		var settings = $.extend({
+			autoPlaySpeed : false,
+			btnsText      : {
+				next      : "",
+				prev      : "",
+				pause     : "",
+				play      : ""
+			},
+			hoverPause    : false,
+			navigation    : false,
+			responsive    : false,
+			speed         : "normal",
+			swipe         : false,
+			width         : "100%"
+		}, options);
+		
+		return this.each(function(i, elem){
+			
+			if ($(".slider-box", $(elem)).size() > 1) {
+			
+				var is = {};
+					
+					is.wrapper  = $(elem).width(settings.width);
+					is.current  = 1;
+					is.timer    = false;
+					is.content  = $('<div class="slider-content" style="overflow: hidden;"></div>');
+					is.engine   = $('<div class="slider-engine"></div>');
+					is.next     = $('<span class="slider-btns slider-next">' + settings.btnsText.next + '</span>');
+					is.prev     = $('<span class="slider-btns slider-prev">' + settings.btnsText.prev + '</span>');
+					is.autoPlay = $('<span class="slider-play-toggle" data-pause="' + settings.btnsText.pause + '" data-play="' + settings.btnsText.play + '">' + settings.btnsText.pause + '</span>');
+					is.navList  = $('<ul class="slider-nav"></ul>');
+					is.first    = $(".slider-box", is.wrapper).first().clone().width( is.wrapper.width() );
+					is.last     = $(".slider-box", is.wrapper).last().clone().width( is.wrapper.width() );
+					
+					// Init slider
+					
+					is.engine.append( $(".slider-box", is.wrapper).width( is.wrapper.width() ), is.first ).prepend( is.last );
+					
+					is.content.append( is.engine );
+					is.wrapper.append( is.content, is.prev, is.next );
+					
+					is.count = $(".slider-box", is.wrapper).size();
+
+					is.engine.css({
+						marginLeft : "-" + is.wrapper.width() + "px",
+						width      : is.wrapper.width() * is.count + "px"
+					});
+					
+					// Slider animate fn
+					
+					is.engineAnimate = function (point, curElem) {
+					
+						if (settings.navigation) {
+							$(".slider-nav-items.active", is.wrapper).removeClass("active");
+							$(".slider-nav-items", is.wrapper).eq(curElem - 1).addClass("active");							
+						}
+						
+						if (point == "first") {
+							is.engine.stop(false, true).animate({ marginLeft: "-=" + is.wrapper.width() + "px" }, settings.speed, function (){
+								is.engine.css( "margin-left", "-" + is.wrapper.width() + "px" );
+							});
+						} else if (point == "last") {
+							is.engine.stop(false, true).animate({ marginLeft: "0px" }, settings.speed, function (){
+								is.engine.css( "margin-left", "-" + (is.wrapper.width() * (is.count - 2)) + "px" );
+							});
+						} else {
+							is.engine.stop(false, true).animate({ marginLeft: point + "px" }, settings.speed);
+						}
+						
+					}
+					
+					// Pagination
+					
+					if (settings.navigation) {
+					
+						$(".slider-box", is.wrapper).each(function(i, elem){
+							if (i != (is.count - 1) && i != (is.count - 2)) {
+								is.navList.append($("<li class='slider-nav-items" + ((i == 0) ? " active" : "") + "' data-index='" + (i+1) + "'>" + (i+1) + "</li>"));
+							}
+						});
+						is.wrapper.append( is.navList );
+						
+						$(".slider-nav-items", is.wrapper).click(function(){
+						
+							var dataIndex = $(this).data("index");
+							
+							if (dataIndex != is.current) {										
+								is.engineAnimate( "-" + (is.wrapper.width() * dataIndex), is.current = dataIndex );
+							}
+						});
+						
+					}
+					
+					// Auto play
+					
+					if (settings.autoPlaySpeed) {
+					
+						is.interval = function (autoPlaySpeed) {
+							is.timer = setInterval(function(){
+								(is.current < is.count - 2) ? is.engineAnimate( "-=" + is.wrapper.width(), ++is.current) : is.engineAnimate( "first", is.current = 1 );
+							}, autoPlaySpeed);
+						}
+						$(is.next, is.wrapper).after( is.autoPlay );
+						is.interval(settings.autoPlaySpeed);
+						
+						is.autoPlay.click(function(){
+							if (!is.autoPlay.hasClass("pause")) {
+								is.autoPlay.addClass("pause").text(is.autoPlay.data("play"));
+								clearInterval(is.timer);
+							} else {
+								is.autoPlay.removeClass("pause").text(is.autoPlay.data("pause"));
+								is.interval(settings.autoPlaySpeed);
+							}
+						});
+						
+						if (settings.hoverPause) {
+							$(".slider-box, .slider-btns, .slider-nav", is.wrapper).hover(function(){
+								if (!is.autoPlay.hasClass("pause")) {
+									is.autoPlay.addClass("stop").text(is.autoPlay.data("play"));
+									clearInterval(is.timer);
+								}
+							}, function(){
+								if (!is.autoPlay.hasClass("pause")) {
+									is.autoPlay.removeClass("stop").text(is.autoPlay.data("pause"));
+									is.interval(settings.autoPlaySpeed);
+								}
+							});
+						}
+					}
+					
+					// Responsive
+					
+					if (settings.responsive) {
+					
+						$(window).resize(function(){
+							var selfWidth = $(this).width() + "px";
+							is.wrapper.width( selfWidth );
+							is.engine.stop(false, true).css({
+								marginLeft : "-" + (selfWidth * is.current),
+								width      : selfWidth * is.count
+							});
+							$(".slider-box", is.wrapper).width( selfWidth );
+						});
+						
+					}
+					
+					// Slide buttons
+					
+					is.next.click(function(){
+						(is.current < is.count - 2) ? is.engineAnimate( "-=" + is.wrapper.width(), ++is.current) : is.engineAnimate( "first", is.current = 1 );
+					});
+					
+					is.prev.click(function(){								
+						(is.current > 1) ? is.engineAnimate( "+=" + is.wrapper.width(), --is.current ) : is.engineAnimate( "last", is.current = is.count - 2 );
+					});
+					
+					// Swipe
+					
+					if (settings.swipe) {
+						is.engine.swipe({
+							swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+								(direction == "left") ? is.next.click() : (direction == "right") ? is.prev.click() : null;
+							},
+							threshold:0,
+							fingers:'all'
+						});
+					}
+			
+			}
+			
+		});
+	};
+	
+})(jQuery);
